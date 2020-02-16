b9c0bd4a44669389ac3eb190fdc0c7f4942ec473
Rory Murray <rory.christian.murray@gmail.com>
2020-02-16 10:02:34 -0500

    Add annotations

--- Inbox.sol
+++ Inbox.sol
@@ -1,12 +1,19 @@
 pragma solidity ^0.4.17;
 
 contract Inbox {
+    // state
     string public message;
     
+    // has same name as contract
+    // known as constructor function
+    // called automatically one time 
+    // when contract is deployed
     function Inbox(string initialMessage) public {
         message = initialMessage;
         
     }
+    
+    // getters and setterws
     
     function setMessage(string newMassage) public {
         message = newMassage;
